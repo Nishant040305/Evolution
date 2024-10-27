717d42722a8770557a8b2ca15baeaa74de929e7b
Aryan10 <aryan.shivam.10@gmail.com>
2024-10-27 21:10:04 +0530

    Added express

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-/*server*/
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
