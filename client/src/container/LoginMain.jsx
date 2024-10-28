4827094382c38a8c049e37f33bc8880b0b51cd4b
Nishant <nishant040305@gmail.com>
2024-10-28 12:08:15 +0530

    feat: add react-router-dom library

---
+++ client/src/container/LoginMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+const bgimg = import.meta.process.env.backgroundImage
+const LoginMain = () => {
+    console.log(bgimg);
+  return (
+    <div >
+      
+    </div>
+  )
+}
+
+export default LoginMain
