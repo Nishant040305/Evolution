0561d656c64eeb2ac730f0db24a0461627cfd224
Nishant <nishant040305@gmail.com>
2024-10-29 17:03:11 +0530

    fix: Fix the import statement in App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import url from './url.json';
 import LoginMain from './container/LoginMain';
-import WebsiteBuilder from "./components/MainPage/WebsiteBuilder";
+import WebsiteBuilder from "./container/WebsiteBuilder";
 
 function App() {
   return(
