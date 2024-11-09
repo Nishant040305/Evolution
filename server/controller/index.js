75ef5fc0acd3c7d1e2ffa9a3a03e7a3eb08872b2
Aryan10 <aryan.shivam.10@gmail.com>
2024-11-10 00:10:50 +0530

    feat: Project and permission authorization middleware

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,9 +1,11 @@
 const userController = require('./userController');
 const projectController = require('./projectController');
+const memberController = require('./memberController');
 const credController = require('./credController');
 
 module.exports = {
   projectController,
+  memberController,
   credController,
   userController,
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { userController } = require('../controller');
+const { UserVerifier } = require('../middleware/credMiddleware');
 
 const {
     getAllUserProjects,
@@ -9,6 +10,6 @@ const {
 
 // @route    GET /api/user/:id/project
 // @desc     Get all projects by user id
-router.get('/:id/project', getAllUserProjects);
+router.get('/:id/project', UserVerifier, getAllUserProjects);
 
 module.exports = router;
