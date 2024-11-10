ef853be688d5319f7c9b0096e78c0790e96be43a
Aryan10 <aryan.shivam.10@gmail.com>
2024-11-10 07:03:08 +0530

    feat: Added tips

--- client/src/components/MainPage/BottomBar.jsx
+++ client/src/components/MainPage/BottomBar.jsx
@@ -47,7 +47,7 @@ const BottomBar = () => {
         <div className="flex items-center space-x-4">
           <button className="flex items-center text-gray-600 hover:text-gray-800">
             <Timer className="w-5 h-5 mr-1" />
-            <span className="text-sm">Auto-save enabled</span>
+            <span className="text-sm">Auto-save disabled</span>
           </button>
 
           <button
