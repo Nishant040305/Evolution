9f03517dfa3dfe097e494deb1337151714efe792
Darsh Shah <darshsarjushah@gmail.com>
2025-01-26 19:55:26 +0530

    feat: Added the profile page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'github-0': '#161B22',
+        'github-1': '#0E4429',
+        'github-2': '#006D32',
+        'github-3': '#26A641',
+        'github-4': '#39D353',
+      }
+    }
   },
   plugins: [],
 };
